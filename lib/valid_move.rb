03c415db0dd9adf88908f9eda802b6a96960a31c
Scott Schindler <scottschindler29@gmail.com>
2018-07-24 23:15:21 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,17 +1,10 @@
-# code your #valid_move? method here
-
+def valid_move? (board,index)
+  
 
 def position_taken? (board,index)
-  
   if board[index] == " " || board[index] == "" || board[index] == nil 
-  
     return false
-    
   elsif board[index] == "X" || board[index] == "O"
-  
     return true
-  
   end
-  
-  
 end
