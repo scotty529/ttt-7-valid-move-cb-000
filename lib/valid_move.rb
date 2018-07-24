01fa2859b2c611b4cd176e16f458273f56766c57
Scott Schindler <scottschindler29@gmail.com>
2018-07-24 23:23:49 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,13 +1,13 @@
 def valid_move? (board,index)
   
-  if index.between?(0,8) && !position_taken? (board,index)
+  if index.between?(0,8) && !position_taken?(board,index)
     return true
   end 
   
 end
   
 
-def position_taken? (board,index)
+def position_taken?(board,index)
   if board[index] == " " || board[index] == "" || board[index] == nil 
     return false
   elsif board[index] == "X" || board[index] == "O"
